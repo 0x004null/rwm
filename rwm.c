72af5f42db4f3090b71bfd780dde4754fdedb9da
0x004null <elmadisamir2002@gmail.com>
2025-06-21 21:26:37 +0000

    Update rwm.c

--- rwm.c
+++ rwm.c
@@ -1,1 +1,59 @@
 
+
+#include<X11>
+
+
+
+
+/*
+; rwm:red window manager.
+; an open source project designed by _0x004null_* , SAID-OSS.
+; with a pure c code, focusing on minimality, flexibility, make it easier to interact with machine.
+;
+;
+;
+;
+;
+
+*/
+
+
+#define rwm
+
+
+
+int main(void){
+  signed int;
+  signed int;
+  signed int;
+  signed int;
+
+  unsigned double;
+  unsigned double;
+  char* _low ;
+  
+
+}
+
+void{
+  if else{
+    
+  }
+}
+void{}
+void{}
+void{}
+
+
+
+
+class 
+class
+class
+class
+class
+class
+class
+class
+class
+class
